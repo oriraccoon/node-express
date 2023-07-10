7d96a0fd07617d296ef6f1ae7a8c03c1bedaed30
oriraccoon <102132725+oriraccoon@users.noreply.github.com>
2023-07-10 21:38:24 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,172 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const nunjucks = require('nunjucks');
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Modal from '@mui/material/Modal';
+import { styled } from '@mui/system';
+import Slide from '@mui/material/Slide';
 
-var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'njk');
-nunjucks.configure('views', { 
-  express: app,
-  watch: true,
+const ModalBox = styled(Box)({
+  width: 400,
+  background: '#ffffff',
+  boxShadow: 24,
+  borderRadius: 10,
+  padding: 10,
+  outline: 'none',
 });
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [signup, setSignup] = useState({ username: '', password: '' });
+  const [open, setOpen] = useState(false);
+  const [signupUsernameError, setSignupUsernameError] = useState(false);
+  const [signupPasswordError, setSignupPasswordError] = useState(false);
 
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log('Login with', username, password);
+  };
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  const handleSignupChange = (name, value) => {
+    setSignup({ ...signup, [name]: value });
+    if (name === 'username' && value.length < 5) {
+      setSignupUsernameError(true);
+    } else if (name === 'username') {
+      setSignupUsernameError(false);
+    }
+    if (name === 'password' && value.length < 8) {
+      setSignupPasswordError(true);
+    } else if (name === 'password') {
+      setSignupPasswordError(false);
+    }
+  };
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  const handleSignup = (event) => {
+    event.preventDefault();
+    if (!signupUsernameError && !signupPasswordError) {
+      console.log('Signup with', signup.username, signup.password);
+      setOpen(false);
+    }
+  };
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-module.exports = app;
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 12,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Man tracking Sensor
+        </Typography>
+        <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoFocus
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3}}
+          >
+            Login
+          </Button>
+          <Button
+            fullWidth
+            variant="contained"
+            sx={{ mt: 2}}
+            onClick={handleOpen}
+          >
+            Signup
+          </Button>
+        </Box>
+      </Box>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Slide direction="down" in={open}>
+          <ModalBox>
+            <Typography variant="h6" component="h2">
+              Signup
+            </Typography>
+            <form onSubmit={handleSignup}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="signupUsername"
+                label="Username"
+                name="username"
+                autoFocus
+                error={signupUsernameError}
+                helperText={signupUsernameError ? '사용자 이름은 5글자 이상이어야 합니다.' : ''}
+                value={signup.username}
+                onChange={(e) =>
+                  handleSignupChange('username', e.target.value)
+                }
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="signupPassword"
+                error={signupPasswordError}
+                helperText={signupPasswordError ? '사용자 비밀번호는 8글자 이상이어야 합니다.' : ''}
+                value={signup.password}
+                onChange={(e) =>
+                  handleSignupChange('password', e.target.value)
+                }
+              />
+              <Button type="submit" fullWidth variant="contained" sx={{ mt: 3 }}>
+                Sign Up
+              </Button>
+            </form>
+          </ModalBox>
+        </Slide>
+      </Modal>
+
+    </Container>
+  );
+}
+
+export default App;
